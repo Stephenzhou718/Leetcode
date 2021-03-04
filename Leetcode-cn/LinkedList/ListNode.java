56a061f371ca89861b4267625beb4186889ea3ea
zhouhanming <1436990010@qq.com>
2021-03-04 23:21:05 +0800

    ADD: add reverse linked list

---
+++ Leetcode-cn/LinkedList/ListNode.java
@@ -0,0 +1,13 @@
+package LinkedList;
+
+/**
+ * @Author: zhouhanming
+ * @Date:2021/3/4 10:43 下午
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) {this.val = val;}
+    ListNode(int val, ListNode next) {this.val = val; this.next = next;}
+}
---
+++ Leetcode-cn/LinkedList/ReverseLinkedList.java
@@ -0,0 +1,43 @@
+package LinkedList;
+
+/**
+ * @Author: zhouhanming
+ * @Date:2021/3/4 10:40 下午
+ *
+ * https://leetcode-cn.com/problems/reverse-linked-list/
+ *
+ * 解法：
+ * 1. 迭代
+ * 2. 递归
+ * 3. 栈
+ *
+ * 五毒神掌：第二遍， 第三遍时间：2021/3/5
+ */
+public class ReverseLinkedList {
+
+    // 递归
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+
+        ListNode next = head.next;
+        ListNode node = reverseList(next);
+        head.next = null;
+        next.next = head;
+        return node;
+    }
+
+    // 循环
+    public ListNode reverseList_2(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
