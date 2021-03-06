b2825d79b8acaae8b59983efb8be2dade4a1cd50
zhouhanming <1436990010@qq.com>
2021-03-06 14:46:28 +0800

    ADD: add linked list cycle

---
+++ Leetcode-cn/LinkedList/LinkedListCycle.java
@@ -0,0 +1,50 @@
+package LinkedList;
+
+import java.util.HashSet;
+
+/**
+ * @Author: zhouhanming
+ * @Date:2021/3/5 9:23 下午
+ * https://leetcode-cn.com/problems/linked-list-cycle/
+ * <p>
+ * 思路：
+ * 1. 把走过的 Node 都记录下来：时间复杂度 O(n), 空间复杂度 O(1)
+ * 2. 快慢指针：时间复杂度 O(n), 空间复杂度 O(1)
+ * <p>
+ * 五毒伸张：第二遍， 第三遍时间 2021/3/6
+ */
+public class LinkedListCycle {
+
+    // 快慢指针
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // 记录 Node
+    public boolean hasCycle_2(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+
+        HashSet<ListNode> nodeSet =  new HashSet<>();
+        while (head != null) {
+            if (nodeSet.contains(head)) {
+                return true;
+            }
+            nodeSet.add(head);
+            head = head.next;
+        }
+
+        return false;
+    }
+
+}
