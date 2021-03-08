5d0029512077207bfb2e385964d6199fbd6cc0f1
zhouhanming <1436990010@qq.com>
2021-03-08 23:25:53 +0800

    ADD: sliding window

--- Leetcode-cn/Queue/SlidingWindowMaximum.java
+++ Leetcode-cn/Queue/SlidingWindowMaximum.java
@@ -1,5 +1,7 @@
 package Queue;
 
+import java.util.*;
+
 /**
  * @Author: zhouhanming
  * @Date:2021/3/7 10:53 下午
@@ -7,7 +9,36 @@ package Queue;
  *
  * 思路：
  * 1. 用 Queue 模拟数组：时间复杂度 O(n^2), 空间复杂度 O(k)
- * 2.
+ * 2. 双端队列：时间复杂度 O(n), 空间复杂度 O(k)
+ *
+ * 五毒神掌：第二遍，第三遍时间 2021/3/8
  */
 public class SlidingWindowMaximum {
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || nums.length < k) {
+            return nums;
+        }
+
+        LinkedList<Integer> queue = new LinkedList<>();
+        int[] res = new int[nums.length - k + 1];
+        int index = 0;
+        for (int i = 0; i < nums.length; i++) {
+            while (!queue.isEmpty() && nums[i] >= nums[queue.peekLast()]) {
+                queue.pollLast();
+            }
+            queue.addLast(i);
+
+            if (queue.peekFirst() <= i - k) {
+                queue.pollFirst();
+            }
+
+            if (i >= k - 1) {
+                res[index++] = nums[queue.peekFirst()];
+            }
+        }
+        return res;
+    }
+
+
 }
