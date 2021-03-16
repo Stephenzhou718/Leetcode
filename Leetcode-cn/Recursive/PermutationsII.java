b14cf3f1b3be1e8130bba96d7f4a8493c807b2c7
zhouhanming <1436990010@qq.com>
2021-03-16 21:46:41 +0800

    ADD: add permulation II

---
+++ Leetcode-cn/Recursive/PermutationsII.java
@@ -0,0 +1,56 @@
+package Recursive;
+
+import java.util.*;
+
+/**
+ * @Author: zhouhanming
+ * @Date:2021/3/14 11:23 下午
+ * https://leetcode-cn.com/problems/permutations-ii/
+ *
+ * 思路：
+ * dfs:
+ *
+ * 五毒神掌：
+ * 第二遍，2021/3/14
+ *
+ */
+public class PermutationsII {
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        int len = nums.length;
+        List<List<Integer>> res = new ArrayList<>();;
+        if (len == 0) {
+            return res;
+        }
+
+        Arrays.sort(nums);
+        boolean[] used = new boolean[len];
+        Deque<Integer> path = new ArrayDeque<>();
+        dfs(nums, len, 0, used, path, res);
+        return res;
+    }
+
+    public void dfs(int[] nums, int len, int depth, boolean[] used, Deque<Integer> path, List<List<Integer>> res) {
+        if (depth == len) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = 0; i < len; i++) {
+            if (used[i]) {
+                continue;
+            }
+
+            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) {
+                continue;
+            }
+
+            path.add(nums[i]);
+            used[i] = true;
+            dfs(nums, len, depth + 1, used, path, res);
+            path.removeLast();
+            used[i] = false;
+        }
+    }
+
+}
