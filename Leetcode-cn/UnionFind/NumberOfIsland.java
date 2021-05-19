55954d56ba3c988c1dd8334bfefe2820b0e618b5
zhouhanming <1436990010@qq.com>
2021-05-19 09:32:53 +0800

    ADD: add unionFind 2th practice

---
+++ Leetcode-cn/UnionFind/NumberOfIsland.java
@@ -0,0 +1,91 @@
+package UnionFind;
+
+/**
+ * @Author: zhouhanming
+ * @Date:2021/5/18 11:31 下午
+ * https://leetcode-cn.com/problems/number-of-islands/
+ *
+ * 思路：
+ * 1. DFS
+ * 2. BFS
+ * 3. 并查集
+ *
+ */
+public class NumberOfIsland {
+    public int numIslands(char[][] grid) {
+        if (grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+        UnionFind unionFind = new UnionFind(grid);
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == '1') {
+                    grid[i][j] = '0';
+                    if (i - 1 > 0 && grid[i - 1][j] == '1') {
+                        unionFind.union(i * cols + j, (i - 1) * cols + j);
+                    }
+                    if (i + 1 < rows && grid[i + 1][j] == '1') {
+                        unionFind.union(i * cols + j, (i + 1) * cols + j);
+                    }
+                    if (j - 1 > 0 && grid[i][j - 1] == '1') {
+                        unionFind.union(i * cols + j, i * cols + j - 1);
+                    }
+                    if (j + 1 < cols && grid[i][j + 1] == '1') {
+                        unionFind.union(i * cols + j, i * cols + j +1);
+                    }
+                }
+            }
+        }
+        return unionFind.count;
+    }
+
+    class UnionFind {
+        int count;
+        int[] parent;
+        int[] rank;
+
+        public UnionFind(char[][] grid) {
+            count = 0;
+            int m = grid.length;
+            int n = grid[0].length;
+
+            parent = new int[m * n];
+            rank = new int[m * n];
+
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (grid[i][j] == '1') {
+                        parent[i * n + j] = i * n + j;
+                        ++count;
+                    }
+                }
+            }
+        }
+
+        private int find(int n) {
+            while (n != parent[n]) n = parent[n];
+            return parent[n];
+        }
+
+        private void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX != rootY) {
+                if (rank[rootX] > rank[rootY]) {
+                    parent[rootY] = rootX;
+                } else if (rank[rootX] < rank[rootY]) {
+                    parent[rootX] = rootY;
+                } else {
+                    parent[rootY] = rootX;
+                    rank[rootX]++;
+                }
+                count--;
+            }
+        }
+
+    }
+}
---
+++ Leetcode-cn/UnionFind/SurroundedRegions.java
@@ -0,0 +1,102 @@
+package UnionFind;
+
+/**
+ * @Author: zhouhanming
+ * @Date:2021/5/18 11:52 下午
+ * https://leetcode-cn.com/problems/surrounded-regions/
+ *
+ * 思路：
+ * 1. DFS
+ * 2. BFS
+ * 3. 并查集
+ *
+ * 五毒神掌：
+ * 第二遍，2021/5/19
+ */
+public class SurroundedRegions {
+    public void solve(char[][] board) {
+        if (board.length == 0 || board[0].length == 0) {
+            return;
+        }
+
+        int m = board.length;
+        int n = board[0].length;
+
+        // 用一个虚拟节点，边界上的 O 的父节点都是这个虚拟节点
+        UnionFind unionFind = new UnionFind(m * n + 1);
+        int dummyNode = m * n;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'O') {
+                    if (i == 0 || i == m - 1|| j == 0 || j == n - 1) {
+                        // 便捷上的 O。把它和 dummuNode 合并成一个联通区域
+                        unionFind.union(i * n + j, dummyNode);
+                    } else {
+                        if (i > 0 && board[i - 1][j] == 'O') {
+                            unionFind.union(i * n + j, (i - 1) * n + j);
+                        }
+                        if (i + 1 < m && board[i + 1][j] == 'O') {
+                            unionFind.union(i * n + j, (i + 1) * n + j);
+                        }
+                        if (j > 0 && board[i][j - 1] == 'O') {
+                            unionFind.union(i * n + j, i * n + j - 1);
+                        }
+                        if (j + 1 < n && board[i][j + 1] == 'O') {
+                            unionFind.union(i * n + j, i * n + j + 1);
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (unionFind.isConnected(i * n + j, dummyNode)) {
+                    board[i][j] = 'O';
+                } else {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+
+    class UnionFind {
+        private int count;
+        private int[] parent;
+        private int[] rank;
+
+        public UnionFind (int n) {
+            count = 0;
+            parent = new int[n];
+            rank = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+
+        public int find (int i) {
+            while (i != parent[i]) i = parent[i];
+            return parent[i];
+        }
+
+        public boolean isConnected(int x, int y) {
+            return find(x) == find(y);
+        }
+
+        public void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX != rootY) {
+                if (rank[rootX] > rank[rootY]) {
+                    parent[rootY] = rootX;
+                } else if (rank[rootX] < rank[rootY]) {
+                    parent[rootX] = rootY;
+                } else {
+                    parent[rootY] = rootX;
+                    rank[rootX]++;
+                }
+            }
+        }
+    }
+}
