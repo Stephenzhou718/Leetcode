825a1bee7bdf72ae1d2b76ff7dcffd84723b8fda
zhouhanming <1436990010@qq.com>
2021-05-27 21:13:19 +0800

    ADD: add LRUCache 2ht practice

---
+++ Leetcode-cn/LRU/LRUCache.java
@@ -0,0 +1,89 @@
+package LRU;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author: zhouhanming
+ * @Date:2021/5/27 8:43 下午
+ * https://leetcode-cn.com/problems/lru-cache/
+ *
+ * 五毒神掌：
+ * 第二遍，2021/5/27
+ */
+public class LRUCache {
+    class DLinkedNode {
+        int key;
+        int value;
+        DLinkedNode pre;
+        DLinkedNode next;
+        public DLinkedNode() {};
+        public DLinkedNode(int key, int val) {this.key = key; this.value = val;}
+    }
+
+    private Map<Integer, DLinkedNode> cache;
+    private int size;
+    private int capacity;
+    private DLinkedNode head, tail;
+
+    public LRUCache(int capacity) {
+        this.size = 0;
+        this.capacity = capacity;
+        this.cache = new HashMap<>();
+        head = new DLinkedNode();
+        tail = new DLinkedNode();
+        head.next = tail;
+        tail.pre = head;
+    }
+
+    public int get(int key) {
+        DLinkedNode node = cache.get(key);
+        if (node == null) {
+            return -1;
+        }
+
+        moveToHead(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        DLinkedNode node = cache.get(key);
+        if (node == null) {
+            DLinkedNode newNode = new DLinkedNode(key, value);
+            addToHead(newNode);
+            cache.put(key, newNode);
+            size++;
+            if (size > capacity) {
+                DLinkedNode tail = removeTail();
+                cache.remove(tail.key);
+                size--;
+            }
+        } else {
+            node.value = value;
+            moveToHead(node);
+        }
+    }
+
+    public void addToHead(DLinkedNode node) {
+        node.next = head.next;
+        node.pre = head;
+        head.next.pre = node;
+        head.next = node;
+    }
+
+    public void removeNode(DLinkedNode node) {
+        node.pre.next = node.next;
+        node.next.pre = node.pre;
+    }
+
+    public void moveToHead(DLinkedNode node) {
+        removeNode(node);
+        addToHead(node);
+    }
+
+    public DLinkedNode removeTail() {
+        DLinkedNode node = tail.pre;
+        removeNode(node);
+        return node;
+    }
+}
