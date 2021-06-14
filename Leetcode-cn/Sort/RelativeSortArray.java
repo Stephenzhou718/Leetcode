5e97a04586520596e2deb13a63ec56de25740f6d
zhouhanming <1436990010@qq.com>
2021-06-14 22:49:58 +0800

    ADD: add sort

---
+++ Leetcode-cn/Sort/RelativeSortArray.java
@@ -0,0 +1,42 @@
+package Sort;
+
+/**
+ * @Author: zhouhanming
+ * @Date:2021/6/8 9:22 上午
+ * https://leetcode-cn.com/problems/relative-sort-array/
+ *
+ * 思路：计数排序
+ *
+ * 五毒神掌：
+ * 第二遍，2021/6/14
+ */
+public class RelativeSortArray {
+
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int upper = 0;
+        for (int x : arr1) {
+            upper = Math.max(upper, x);
+        }
+        int[] frequency = new int[upper + 1];
+        for (int x : arr1) {
+            ++frequency[x];
+        }
+        int[] ans = new int[arr1.length];
+        int index = 0;
+        for (int x : arr2) {
+            for (int i = 0; i < frequency[x]; i++) {
+                ans[index++] = x;
+            }
+            frequency[x] = 0;
+        }
+
+        for (int x = 0; x <= upper; ++x) {
+            for (int i = 0; i < frequency[x]; ++i) {
+                ans[index++] = x;
+            }
+        }
+
+        return ans;
+    }
+    
+}
---
+++ Leetcode-cn/Sort/ValidAnagram.java
@@ -0,0 +1,35 @@
+package Sort;
+
+import java.util.Arrays;
+
+/**
+ * @Author: zhouhanming
+ * @Date:2021/6/8 9:22 上午
+ * https://leetcode-cn.com/problems/valid-anagram/
+ *
+ * 有效的字母异位词，
+ * 思路：哈希表，计数排序， 排序再比较
+ *
+ * 五毒神掌：
+ * 第二遍：2021/6/14
+ */
+public class ValidAnagram {
+
+    public boolean isAnagram(String s, String t) {
+        if (s == null && t == null) {
+            return true;
+        } else if (s == null || t == null) {
+            return false;
+        } else if (s.length() != t.length()) {
+            return false;
+        }
+
+        char[] arr1 = s.toCharArray();
+        char[] arr2 = t.toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1, arr2);
+    }
+}
